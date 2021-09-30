804be22e1ba6a88da10377c25a09e5febbbd65e2
Dilshod <exaplele@gmail.com>
2021-09-30 17:21:12 +0500

    first commit

---
+++ js/skript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// const answer = +prompt("вам есть 18?", "18");
+// console.log(typeof(answer) );
+const answers = [];
+
+answers[0] = prompt('как ваше имя?');
+answers[1] = prompt('как ваше фамилия?');
+answers[2] = prompt('сколько авм лет?');
+document.write(answers);
