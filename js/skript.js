d39156d3f8c862b434e291dd4904c83efb959b38
Dilshod <exaplele@gmail.com>
2021-10-12 18:13:32 +0500

    dilshod

--- js/skript.js
+++ js/skript.js
@@ -1,21 +1,31 @@
 "use strict";
 
-const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('сколько фильмов вы уже посмотрели?', ''),
-    b = prompt('на сколько оцените эго?'),
-    c = prompt('сколько фильмов вы уже посмотрели?', ''),
-    d = prompt('на сколько оцените эго?');
+// const a = prompt('сколько фильмов вы уже посмотрели?', ''),
+//     b = prompt('на сколько оцените эго?'),
+//     c = prompt('сколько фильмов вы уже посмотрели?', ''),
+//     d = prompt('на сколько оцените эго?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+// if (4 == 5) {
+//     console.log('ok!');
+// } else {
+//     console.log('eror');
+// }
+let num = 1;
+for (let i = 1; i < 10; i++) {
+    console.log(num);
+    num++;
+}
